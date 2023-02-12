1cb30be9dc294baed0df9e73cce7d39c007044d1
JorgeRomero01 <jrome140@fiu.edu>
2023-02-12 00:08:25 -0500

    ...

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -14,12 +14,25 @@ public class Employee {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name  = "id")
 	private int id;
-	@Column(name = "firstName")
+	@Column(name = "first_name")
 	private String firstName;
-	@Column(name = "lastName")
+	@Column(name = "last_name")
 	private String lastName;
 	@Column(name = "email")
 	private String email;
+	
+	//define constructor
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
 	
 
 }
