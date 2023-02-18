dae4dd9dec161fc62ac8db5dee964a24b696c6ce
JorgeRomero01 <jrome140@fiu.edu>
2023-02-18 17:32:16 -0500

    ...

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,9 +44,22 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 	
-	@PostMapping("/employees/{theId}")
+	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		theEmployee.setId(0);
 		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
 	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	
 
 }
