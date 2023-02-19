a043f0adb4560b2152bc3e4c195aefb869f19fd7
JorgeRomero01 <jrome140@fiu.edu>
2023-02-18 19:09:00 -0500

    :)

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,16 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 	
-	
-
+	@DeleteMapping("/employees/{theId}")
+	public String deleteEmployee(@PathVariable int theId) {
+		Employee tempEmployee = employeeService.findById(theId);
+		
+		if(tempEmployee == null) {
+			throw new RuntimeException("Employee Id not found - " + theId);
+		}
+		
+		employeeService.delete(theId);
+		
+		return "Deleted employee id - " + theId;
+	}
 }
